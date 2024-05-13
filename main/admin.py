7c6616a3e39d7690773d4acedce47ce23b0f58ad
alexcy <bondaralexcy@gmail.com>
2024-05-14 01:11:05 +0300

    Try to admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from main.models import Category, Product
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'category_name',)
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'product_name', 'price', 'category', 'descr')
+    # Добавляем поля для фильтрации
+    list_filter = ('category',)
+
+    # Добавляем поля для поиска
+    search_fields = ('product_name', 'descr',)
